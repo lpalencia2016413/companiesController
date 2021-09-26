5760a677b8d53ddd75b89b6d0b9b832d2fc0e1a7
lpalencia2016413 <lpalencia-2016413@kinal.edu.gt>
2021-09-26 16:40:53 -0600

    Agregando global.service conexion a heroku

--- front/src/app/services/company.service.ts
+++ front/src/app/services/company.service.ts
@@ -1,9 +1,1 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class CompanyService {
-
-  constructor() { }
-}
--- front/src/app/services/global.service.ts
+++ front/src/app/services/global.service.ts
@@ -1,9 +1,4 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class GlobalService {
-
-  constructor() { }
+export var GlobalService = {
+  //url: "http://localhost:3000/api/"
+  url: "https://companies-controller.herokuapp.com/api/"
 }
